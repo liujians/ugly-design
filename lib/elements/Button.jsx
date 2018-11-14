d5087add55f0d6ec8b8c7c8000e1a3b15641da8c
刘剑 <liujian@magicare.me>
2018-11-14 15:28:55 +0800

    「update」init

---
+++ lib/elements/Button.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+// import * as PropTypes from 'prop-types';
+
+export default class Button extends React.Component {
+  render() {
+    return (
+      <button style={{ background: 'red' }} type="button">
+        button
+      </button>
+    );
+  }
+}
